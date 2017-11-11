8f521b0e1762c896c3af793c0a6fdad8bc5e0657
Hugo Hörnquist <hugo@hornquist.se>
2017-11-11 12:06:50 +0100

    started breaking SDL stuff into own file

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 
 #include "types.h"
 #include "event.h"
+#include "sdl_scm.h"
 
 bool ready = false;
 SCM draw_func = SCM_UNDEFINED;
@@ -85,49 +86,6 @@ static void* call_funcs (void* args) {
 
 	return NULL;
 }
-
-static SCM draw_rect (SCM fill_p, SCM x, SCM y, SCM w, SCM h) {
-	SDL_Rect rect = {.x = scm_to_int (x),
-	                 .y = scm_to_int (y),
-					 .w = scm_to_int (w),
-					 .h = scm_to_int (h) };
-	(scm_is_true (fill_p) ? SDL_RenderFillRect : SDL_RenderDrawRect)
-		(renderer, &rect);
-	return SCM_UNDEFINED;
-}
-
-static SCM set_color (SCM r, SCM g, SCM b, SCM a) {
-	if (SCM_UNBNDP (a))
-		a = scm_from_int(0xFF);
-
-	SDL_SetRenderDrawColor (renderer,
-			scm_to_int(r),
-			scm_to_int(g),
-			scm_to_int(b),
-			scm_to_int(a));
-
-	return SCM_UNDEFINED;
-}
-
-TTF_Font* sans;
-SDL_Color white = {0xFF, 0xFF, 0xFF};
-static SCM draw_text (SCM text, SCM _x, SCM _y) {
-	int x = scm_to_int (_x);
-	int y = scm_to_int (_y);
-	char* str = scm_to_utf8_string (text);
-	SDL_Surface* surfaceMessage = TTF_RenderText_Blended(sans, str, white);
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
-
-	SDL_Rect rect = {.x = x, .y = y, .w = surfaceMessage->w, .h = surfaceMessage->h };
-
-	SDL_RenderCopy(renderer, texture, NULL, &rect);
-
-	SDL_FreeSurface(surfaceMessage);
-	SDL_DestroyTexture(texture);
-
-	return SCM_UNDEFINED;
-}
-
 /*
  * These are defined here to allow for the guile thread
  * to acccess them upon startup.
---
+++ sdl_scm.h
@@ -0,0 +1,16 @@
+#ifndef SDL_SCM_H_
+#define SDL_SCM_H_
+
+#include <libguile.h>
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+
+SCM draw_rect (SCM fill_p, SCM x, SCM y, SCM w, SCM h);
+SCM set_color (SCM r, SCM g, SCM b, SCM a);
+TTF_Font* sans;
+//SDL_Color white = {0xFF, 0xFF, 0xFF};
+SCM draw_text (SCM text, SCM _x, SCM _y);
+extern SDL_Renderer* renderer;
+
+#endif // SDL_SCM_H_
