37182fbeeaea60885cd5dd30dc17ab05c8c1a0e4
Hugo Hörnquist <hugo@hornquist.se>
2018-01-25 22:38:59 +0100

    scene 2 migrated to use my macros, also cleaned up

--- scheme/scene/scene2.scm
+++ scheme/scene/scene2.scm
@@ -5,7 +5,6 @@
                #:use-module (vector)
                #:use-module (objects ellipse)
                #:use-module (objects character)
-               ;;#:use-module (objects vecloader)
 
                #:use-module (collide)
 
@@ -14,18 +13,6 @@
                #:export (eel pel test-character
                          scene2
                              ))
-
-#|
-(define coll-text (make <text-obj>
-                        #:pos (make <v2> #:y 5 #:x 5)
-                        #:str "U"
-                        #:update (lambda (_)
-                                   (if (colliding? pel eel)
-                                     "T" "F"))))
-
-(register-draw-object! coll-text)
-(register-tick-object! coll-text)
-|#
 
 #| TODO
  | Quite a lot of stuff here should be out-macro'd
@@ -39,60 +26,45 @@
  | - Clear way to mark stuff that should be reloaded
  |#
 
+#| Secound TODO
+ | I don't want to recreate the objects in the
+ | scene on reload. But I want to update them
+ | to contain everything new. This is possibly
+ | done automaticly. Otherwise something like
+ | a class replace should suffice.
+ |#
 
-(define-once scene2 (make <scene> #:name "SCENE 2"))
+(with-new-scene
+  scene2 "SCENE 2"
+  (define-class <ctrl-el> (<ellipse> <arrow-control>))
 
-(with-scene scene2
-(define-class <ctrl-el> (<ellipse> <arrow-control>))
-(define-once pel
-  (make <ctrl-el>
-        #:name "[ELLIPSE 1]"
-        #:pos (make <v2> #:x 40 #:y 40)
-        #:r 100
-        #:d 30))
+  (define-once pel
+    (make <ctrl-el>
+          #:name "[ELLIPSE 1]"
+          #:pos (make <v2> #:x 40 #:y 40)
+          #:r 100
+          #:d 30))
 
-(define-once eel
-  (make <ellipse>
-        #:name "[ELLIPSE 2]"
-        #:pos (make <v2> #:x 120 #:y 120)
-        #:color '(#xFF 0 0)
-        #:r 100
-        #:d 30))
+  (define-once eel
+    (make <ellipse>
+          #:name "[ELLIPSE 2]"
+          #:pos (make <v2> #:x 120 #:y 120)
+          #:color '(#xFF 0 0)
+          #:r 100
+          #:d 30))
 
-;; (define-once figure
-;;   (parse (open-input-file "assets/obj.sxml")))
+  (define f (open-input-file "assets/obj.sxml"))
+  (define test-character
+    (make <character>
+          #:file (open-input-file "assets/obj.sxml")
+          #:pos (make <v2> #:x 200 #:y 200)))
+  
+  (do-once
+    (register-tick-object! pel)
+    (register-draw-object! pel)
 
-(define f (open-input-file "assets/obj.sxml"))
-;;; (define c (create-character f))
-(define test-character
-  (make <character>
-        #:file (open-input-file "assets/obj.sxml")
-        #:pos (make <v2> #:x 200 #:y 200)))
+    (register-draw-object! eel)
 
-(define-once *loaded* #f)
+    (register-draw-object! test-character)
 
-;; TODO create a load-once macro
-(unless *loaded*
-  (set! *loaded* #t)
-
-  ;(register-draw-object! pel)
-  ;;(register-event-object! pel)
-  (register-tick-object! pel)
-  (register-draw-object! pel)
-
-  (register-draw-object! eel)
-
-  (register-draw-object! test-character)
-
-  ;(register-draw-object! eel)
-  ;;(register-event-object! eel)
-  ;;(register-tick-object! eel)
-  )
-
-;;;(define-method (tick-func (obj <ctrl-el>))
-;;;               (collide! obj eel))
-
-;;               (let ((other eel))
-;;                 (if (not (eq? obj other))
-;;                   (collide! obj other))))
-)
+))
