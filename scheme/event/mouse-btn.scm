4d265c69df2ff56bb94bb4f36aea8cefc9bd0422
Hugo Hörnquist <hugo@hornquist.se>
2018-06-24 16:09:51 +0200

    Add rclick? for <mouse-btn-event>.

--- scheme/event/mouse-btn.scm
+++ scheme/event/mouse-btn.scm
@@ -1,13 +1,14 @@
 (define-module (event mouse-btn)
   #:use-module (event)
   #:use-module (oop goops)
+  #:use-module (ice-9 curried-definitions)
   ;; Something with this re-export doesn't work as expected
   #:re-export (<mouse-button-event> make-mouse-button-event pos)
   #:export (*mouse-left-btn*
 	    *mouse-middle-btn*
 	    *mouse-right-btn* 
 
-	    lclick?
+	    lclick? rclick?
 	    ))
 
 (define *mouse-left-btn*   1)
@@ -15,7 +16,10 @@
 (define *mouse-right-btn*  3)
 
 ;; this should be a <mouse-btn-event>
-(define (lclick? this)
-  "Is the mouse event a left click"
-  (and (= *mouse-left-btn* (slot-ref this 'button))
-       (eqv? 'SDL_MOUSEBUTTONUP (slot-ref this 'type))))
+
+(define ((click? btn) event)
+  (and (= btn (slot-ref event 'button))
+       (eqv? 'SDL_MOUSEBUTTONUP (slot-ref event 'type))))
+
+(define lclick? (click? *mouse-left-btn*))
+(define rclick? (click? *mouse-right-btn*))
